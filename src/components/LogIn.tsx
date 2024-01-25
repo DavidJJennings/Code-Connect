1c2bc9dfd0645d4a8c5fed87b57d0c7326b86d74
DavidJJennings <103413442+DavidJJennings@users.noreply.github.com>
2024-01-25 21:51:09 +0000

    Header, Hero and responsivity added to LogIn component

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -12,6 +12,12 @@ const LogIn = () => {
           <SignIn>Sign In</SignIn>
         </div>
       </Nav>
+      <Section>
+        <Hero>
+          <h1>Connect, Create, Conquer!</h1>
+          <img src="/Login-Hero.png" alt="Background Image of Atom" />
+        </Hero>
+      </Section>
     </Container>
   );
 };
@@ -21,7 +27,7 @@ const Container = styled.div`
 `;
 
 const Nav = styled.nav`
-  max-width: 1440px;
+  max-width: 1280px;
   padding: 2rem 0 2rem 0;
   margin: auto;
   display: flex;
@@ -34,20 +40,22 @@ const Nav = styled.nav`
     width: 15rem;
     height: 5rem;
     @media (max-width: 768px) {
-      padding: 5rem 2rem;
     }
   }
 `;
 
 const Join = styled.a`
-  font-size: 1rem;
+  font-size: 1.25rem;
   color: #34495e;
   padding: 0.5rem 0.75rem;
   font-family: TabarraPro-black, sans-serif;
-  margin-right: 0.75rem;
-  border: 3px solid black;
+  margin-right: 2rem;
+  box-shadow: inset 0 0 0 2px #34495e;
+  text-decoration: none;
   border-radius: 44px;
-  border-color: #34495e;
+  padding: 0.75rem 1rem;
+  text-align: center;
+  transition-duration: 300ms;
   cursor: pointer;
   &:hover {
     background-color: rgba(52, 73, 94, 0.3);
@@ -55,18 +63,74 @@ const Join = styled.a`
 `;
 
 const SignIn = styled.a`
-  font-size: 1rem;
+  font-size: 1.25rem;
   color: white;
   padding: 0.5rem 0.75rem;
   font-family: TabarraPro-black, sans-serif;
   margin-right: 0.75rem;
-  border: 3px solid black;
   border-radius: 44px;
-  border-color: #34495e;
+  padding: 0.75rem 1rem;
   background-color: #34495e;
+  text-decoration: none;
+  text-align: center;
+  transition-duration: 300ms;
   cursor: pointer;
   &:hover {
     background-color: rgba(66, 69, 72, 0.3);
+  }
+`;
+
+const Section = styled.section`
+  display: flex;
+  align-content: start;
+  min-height: 600px;
+  padding-top: 5rem;
+  padding-bottom: 7.5rem;
+  position: relative;
+  flex-wrap: wrap;
+  width: 100%;
+  max-width: 1280px;
+  margin: auto;
+
+  @media (max-width: 768px) {
+    padding-top: 2rem;
+  }
+`;
+
+const Hero = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  h1 {
+    width: 65%;
+    color: #34495e;
+    font-size: 3.5rem;
+    font-family: TabarraPro-Light;
+    line-height: 100px;
+    @media (max-width: 768px) {
+      font-size: 2rem;
+      text-align: center;
+      width: 100%;
+    }
+  }
+
+  img {
+    width: 600px;
+    position: absolute;
+    top: 150px;
+
+    right: -120px;
+
+    @media (min-width: 761px) and (max-width: 1000px) {
+      width: 500px;
+      top: 250px;
+    }
+
+    @media (max-width: 760px) {
+      width: 100px;
+      position: initial;
+      margin: auto;
+    }
   }
 `;
 
